2acb19335f774b14ffdf48803e06a98c2e47fc49
wangjunying <wangjunying@uinnova.com>
2021-04-30 15:17:57 +0800

    init app

---
+++ src/index.js
@@ -0,0 +1,8 @@
+/*
+ * @Author: your name
+ * @Date: 2021-04-30 14:37:31
+ * @LastEditTime: 2021-04-30 14:37:31
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \cms\react-admin-client\src\index.js
+ */
